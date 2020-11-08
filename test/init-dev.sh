ffcf99e5a438fd170a67dde37ce74870e0e67949
Robin Breathe <robin@isometry.net>
2020-11-08 16:18:42 +0100

    Improve test environment with dedicated target Docker image

--- test/init-dev.sh
+++ test/init-dev.sh
@@ -2,14 +2,21 @@
 
 set -m
 
-vault server -dev -dev-root-token-id=root &
-sleep 5
+function interrupt_children() {
+    [[ -z "$CONTAINER" ]] || docker stop $CONTAINER >/dev/null
+    wait
+}
+
+trap interrupt_children SIGINT ERR EXIT
+
+env VAULT_ADDR=http://127.0.0.1:8200 vault server -dev -dev-root-token-id=root &
+sleep 1
 
 export VAULT_ADDR=http://127.0.0.1:8200
 
 vault secrets enable ssh
 vault write ssh/config/ca generate_signing_key=true
-vault read -field=public_key ssh-client-signer/config/ca >trusted-user-ca-keys
+vault read -field=public_key ssh/config/ca >trusted-user-ca-keys
 vault secrets tune -max-lease-ttl=300 ssh
 
 vault write ssh/roles/default - <<-EOH
@@ -37,4 +44,15 @@ vault write ssh/roles/root - <<-EOH
     }
 EOH
 
-fg
+echo "# Building vault-ssh-target image"
+docker image build -t vault-ssh-target .
+CONTAINER=$(
+    docker container run --detach --rm \
+        --publish 2222:22 \
+        --volume "$PWD/trusted-user-ca-keys":/etc/ssh/trusted-user-ca-keys \
+        vault-ssh-target
+)
+echo "# STARTED CONTAINER $CONTAINER"
+docker container attach --no-stdin --sig-proxy=false $CONTAINER &
+
+fg %1
